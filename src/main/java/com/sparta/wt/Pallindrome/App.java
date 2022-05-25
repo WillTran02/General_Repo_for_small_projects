710b89c372b62981efd37360589cf17d49f3193a
Will Tran <wtran@spartaglobal.com>
2022-05-25 09:10:15 +0100

    Updated Palindrome App to check sentences for longest Palindromes, and Test app to test functionality of methods in Palindrome App

--- src/main/java/com/sparta/wt/Pallindrome/App.java
+++ src/main/java/com/sparta/wt/Pallindrome/App.java
@@ -1,26 +1,66 @@
 package com.sparta.wt.Pallindrome;
 
-public class App {
-    static String word1 = "Helicopter";
-    static String word2 = "Radar";
+import java.util.ArrayList;
+import java.util.List;
 
+public class App {
+    static String sentence1 = "This is a long sentence that does not contain a palindrome.";
+    static String sentence2 = "This sentence contains a single palindrome: Radar";
+    static String sentence3 = "This sentence has multiple palindromes: ABBA Racecar level reer raCEcar bob";
+    static List<String> palindromeList = new ArrayList<String>() {};
     public static void main(String[] args) {
-        System.out.println(isPalindrome(word1));
-        System.out.println(isPalindrome(word2));
-        //Also, in 1 line
-        System.out.println(new StringBuilder(word1).reverse().toString().equalsIgnoreCase(word1));
+//        System.out.println(isPalindrome(word1));
+//        System.out.println(isPalindrome(word2));
+//        System.out.println(new StringBuilder(word1).reverse().toString().equalsIgnoreCase(word1));
+        pullPalindromesFromSentenceIntoList(sentence1, palindromeList);
+        pullPalindromesFromSentenceIntoList(sentence2, palindromeList);
+        pullPalindromesFromSentenceIntoList(sentence3, palindromeList);
+        int longest = getLongestWordFromList(palindromeList);
+        System.out.println(createListOfLongestStringsOnly(longest, palindromeList));
+    }
+
+    public static List<String> createListOfLongestStringsOnly(int longest, List<String> ls) {
+        List<String> lsToReturn = new ArrayList<String>();
+        for (String word : ls) {
+            if (word.length() == longest) {
+                lsToReturn.add(word);
+//                System.out.println("Palindrome detected: " + word);
+            }
+        }
+        return lsToReturn;
+    }
+
+    public static int getLongestWordFromList(List<String> ls) {
+        int longest = 0;
+        for (String word : ls) {
+            if (word.length() > longest) {
+                longest = word.length();
+            }
+        }
+        return longest;
+    }
+
+    public static void pullPalindromesFromSentenceIntoList(String sentence, List<String> ls) {
+        for (String word : splitSentenceIntoStringArray(sentence)) {
+            if (isPalindrome(word)) {
+                ls.add(word);
+            }
+        }
+    }
+
+    public static String[] splitSentenceIntoStringArray(String sentence) {
+        String[] stringArray = sentence.split(" ");
+        return stringArray;
     }
 
     public static boolean isPalindrome(String wordToCheck) {
-        char[] charArray = wordToCheck.toCharArray();
-        String wordReversed = "";
-        for (int i = charArray.length-1; i >= 0; i--) {
-            wordReversed += charArray[i];
-        }
-//        if (wordToCheck.equalsIgnoreCase(wordReversed))
-//            return true;
-//        else
-//            return false;
-        return wordToCheck.equalsIgnoreCase(wordReversed);
+//        char[] charArray = wordToCheck.toCharArray();
+//        String wordReversed = "";
+//        for (int i = charArray.length-1; i >= 0; i--) {
+//            wordReversed += charArray[i];
+//        }
+//        return wordToCheck.equalsIgnoreCase(wordReversed);
+        if (wordToCheck.length() < 6) return false;
+        return new StringBuilder(wordToCheck).reverse().toString().equalsIgnoreCase(wordToCheck);
     }
 }
--- src/test/java/com/sparta/wt/Pallindrome/AppTest.java
+++ src/test/java/com/sparta/wt/Pallindrome/AppTest.java
@@ -4,18 +4,63 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AppTest {
 
+//    @Test
+//    @DisplayName("Check: false for Helicopter")
+//    void trueTest() {
+//        Assertions.assertEquals(false, App.isPalindrome("Elevation"));
+//    }
+//
+//    @Test
+//    @DisplayName("Check: true for Racecar")
+//    void falseTest() {
+//        Assertions.assertEquals(true, App.isPalindrome("Racecar"));
+//    }
+
     @Test
-    @DisplayName("Check: false for Helicopter")
-    void trueTest() {
-        Assertions.assertEquals(false, App.isPalindrome("Elevation"));
+    @DisplayName("Check: splitting a sentence")
+    void findPalindromeTest() {
+        String[] array = new String[4];
+        array = App.splitSentenceIntoStringArray("This is a sentence");
+        Assertions.assertEquals(true, array[0] != null);
+        Assertions.assertEquals(true, array[1] != null);
+        Assertions.assertEquals(true, array[2] != null);
+        Assertions.assertEquals(true, array[3] != null);
     }
 
     @Test
-    @DisplayName("Check: true for Racecar")
-    void falseTest() {
-        Assertions.assertEquals(true, App.isPalindrome("Racecar"));
+    @DisplayName("Check: pull palindromes from sentence")
+    void pullPalindromeTest() {
+        List<String> ls = new ArrayList<>();
+        App.pullPalindromesFromSentenceIntoList("This has one palindrome: pallap", ls);
+        Assertions.assertEquals(true, ls.get(0).equals("pallap"));
+    }
+
+    @Test
+    @DisplayName("Check: correct longest String from list")
+    void longestStringInListTest() {
+        List<String> ls = new ArrayList<>();
+        ls.add("Racecar");
+        ls.add("Peanuts");
+        ls.add("ABBA");
+        App.getLongestWordFromList(ls);
+        Assertions.assertEquals(true, App.getLongestWordFromList(ls) == 7);
+    }
+
+    @Test
+    @DisplayName("Check: print correct longest String")
+    void createListOfLongestStringsTest() {
+        List<String> ls = new ArrayList<String>();
+        ls.add("a");
+        ls.add("longest");
+        ls.add("other");
+        ls.add("longest");
+        ls.add("a");
+        Assertions.assertEquals(true, App.createListOfLongestStringsOnly(7, ls).size() == 2);
     }
 
 }
